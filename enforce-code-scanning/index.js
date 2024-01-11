b8dd8b570ab45612c078dec8579bd7bfad0a09f2
Brett Logan <lindluni@github.com>
2024-01-10 21:32:58 -0500

    Create Action for enforcing code-scanning
    Signed-off-by: Brett Logan <lindluni@github.com>

---
+++ enforce-code-scanning/index.js
@@ -0,0 +1,156 @@
+const core = require('@actions/core')
+const {Octokit} = require('@octokit/rest')
+const {retry} = require('@octokit/plugin-retry')
+const {throttling} = require('@octokit/plugin-throttling')
+
+const thresholds = {
+    critical: ['critical'],
+    high: ['critical', 'high'],
+    medium: ['critical', 'high', 'medium'],
+    low: ['critical', 'high', 'medium', 'low'],
+    warning: ['critical', 'high', 'medium', 'low', 'warning'],
+    note: ['critical', 'high', 'medium', 'low', 'warning', 'note'],
+    error: ['critical', 'high', 'medium', 'low', 'warning', 'note', 'error']
+}
+
+const newClient = async (token) => {
+    const _Octokit = Octokit.plugin(retry, throttling)
+    return new _Octokit({
+        auth: token,
+        baseUrl: process.env.API_URL,
+        throttle: {
+            onRateLimit: (retryAfter, options, octokit) => {
+                octokit.log.warn(`Request quota exhausted for request ${options.method} ${options.url}`)
+                if (options.request.retryCount === 0) {
+                    octokit.log.info(`Retrying after ${retryAfter} seconds!`)
+                    return true
+                }
+            },
+            onSecondaryRateLimit: (retryAfter, options, octokit) => {
+                octokit.log.warn(`Abuse detected for request ${options.method} ${options.url}`)
+                if (options.request.retryCount === 0) {
+                    octokit.log.info(`Retrying after ${retryAfter} seconds!`)
+                    return true
+                }
+            },
+        }
+    })
+}
+
+const getInput = () => {
+    try {
+        const age = parseInt(core.getInput('age', {required: true, trimWhitespace: true}))
+        const message = core.getInput('message', {required: true, trimWhitespace: true})
+        const org = core.getInput('org', {required: true, trimWhitespace: true})
+        const pr = parseInt(core.getInput('pull_request', {required: true, trimWhitespace: true}))
+        const repo = core.getInput('repo', {required: true, trimWhitespace: true})
+        const threshold = core.getInput('threshold', {required: true, trimWhitespace: true})
+        const token = core.getInput('token', {required: true, trimWhitespace: true})
+
+        return {
+            age: age,
+            org: org,
+            repo: repo,
+            message: message,
+            pr: pr,
+            threshold: threshold,
+            token: token
+        }
+    } catch (e) {
+        throw new Error(`Failed to retrieve input variables: ${e.message}`)
+    }
+}
+
+const getAlerts = async (client, org, repo, threshold, age) => {
+    try {
+        const alerts = []
+        for (const severity of thresholds[threshold]) {
+            const _alerts = await client.paginate('GET /repos/{owner}/{repo}/code-scanning/alerts', {
+                owner: org,
+                repo: repo,
+                state: 'open',
+                severity: severity
+            })
+            alerts.push(..._alerts.map(alert => {
+                let exceedsAge = false
+                const created = new Date(alert.created_at)
+                const now = new Date()
+                const diff = now - created
+                const days = Math.floor(diff / (1000 * 60 * 60 * 24))
+                if (days > age) {
+                    exceedsAge = true
+                }
+                return {
+                    number: alert.number,
+                    html_url: alert.html_url,
+                    exceedsAge: exceedsAge,
+                    age: days
+                }
+            }))
+        }
+
+        return alerts
+    } catch (e) {
+        throw new Error(`Failed to retrieve code scanning alerts: ${e.message}`)
+    }
+}
+
+const createComment = async (client, org, repo, pr, message) => {
+    try {
+        await client.issues.createComment({
+            owner: org,
+            repo: repo,
+            issue_number: pr,
+            body: message
+        })
+    } catch (e) {
+        throw new Error(`Failed to create comment: ${e.message}`)
+    }
+
+}
+
+const main = async () => {
+    try {
+        const input = getInput()
+        const client = await newClient(input.token)
+
+        core.info(`Retrieving code scanning alerts for ${input.org}/${input.repo}`)
+        const alerts = await getAlerts(client, input.org, input.repo, input.threshold, input.age)
+        if (alerts.length === 0) {
+            core.info(`No alerts found for ${input.org}/${input.repo}`)
+            return
+        }
+
+        core.info(`Found ${alerts.length} alerts for ${input.org}/${input.repo} with ${input.threshold} threshold`)
+        const summary = core.summary
+            .addHeading(`Code Scanning Policy Findings`)
+            .addRaw(input.message)
+            .addSeparator()
+            .addTable([
+                [
+                    {data: 'Alert Number', header: true},
+                    {data: 'URL', header: true},
+                    {data: 'Age', header: true},
+                    {data: 'Policy Violation', header: true}
+                ],
+                ...alerts.map(alert => [
+                    `${alert.number}`,
+                    `<a href="${alert.html_url}">${alert.html_url}</a>`,
+                    `${alert.age} Days`,
+                    `${alert.exceedsAge ? 'Yes' : 'No'}`
+                ])
+            ]).stringify()
+
+        core.info(`Creating comment for https://${input.org}/${input.repo}/pull/${input.pr}`)
+        await createComment(client, input.org, input.repo, input.pr, summary)
+
+        const violations = alerts.filter(alert => alert.exceedsAge)
+        if (violations.length > 0) {
+            core.setFailed(`Found ${violations.length} policy violations for ${input.org}/${input.repo}`)
+        }
+    } catch (e) {
+        core.setFailed(e.message)
+    }
+}
+
+main()
